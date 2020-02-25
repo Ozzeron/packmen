c2f7150724e25679bdacb4272e1f3936da2c5b23
Ozzeron <nikita.timohin@address.com>
2020-02-25 18:35:25 +0200

    MOVE MTHK

--- pacman.js
+++ pacman.js
@@ -1,21 +1,24 @@
 addEventListener('keydown', (e) => {
-    let currentPackmenPosition = document.getElementById('packmen').attributes.position;
+    let currentPackmenPosition = document.getElementById('pacman').attributes.position;
     let currentApplePosition = document.getElementById('apple');
-    let currentPackmenBox = document.getElementById('pacmen');
+    let currentPackmenBox = document.getElementById('pacman');
     let currentAppleBox = document.getElementById('apple');
     console.log(currentPackmenBox);
     console.log(currentAppleBox);
     switch (e.key) {
         case 'ArrowLeft':
-            currentAppleBox.firstChild.src = 'assets/pac_left.png';
+            currentPackmenBox.firstChild.src = 'assets/pac_left.jpg';
             break;
         case 'ArrowUp':
+            currentPackmenBox.firstChild.src = 'assets/pac_up.png';
             console.log('Кнопка вверх');
             break;
         case 'ArrowRight':
+            currentPackmenBox.firstChild.src = 'assets/pac_right.png';
             console.log('Кнопка вправо');
             break;
         case 'ArrowDown':
+            currentPackmenBox.firstChild.src = 'assets/pac_down.png';
             console.log('Кнопка вниз')
     }
 });
